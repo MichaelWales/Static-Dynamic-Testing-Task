0b2690f15058c6484fffebc5ce9d302b7d86882a
Michael Wales <michaelwales@gmail.com>
2020-04-09 14:22:49 +0100

    Sets up Spec file with MiniTest

---
+++ specs/card_game_spec.rb
@@ -0,0 +1,13 @@
+require("minitest/autorun")
+require('minitest/reporters')
+
+require_relative("../card_game")
+
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class CardGameTest < MiniTest::Test
+
+  
+
+end
